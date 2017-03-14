5e93541199b0bce4a4194b354e541afeb01d9aab
andrius.satkauskas <andrius.satkauskas@gmail.com>
2017-03-14 09:07:35 +0200

    helloWorld

---
+++ src/main/java/lt/vtvpmc/ems/satkauskas/App.java
@@ -0,0 +1,12 @@
+package lt.vtvpmc.ems.satkauskas;
+
+/**
+ * Hello world!
+ * 
+ * @author andrius satkauskas
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
